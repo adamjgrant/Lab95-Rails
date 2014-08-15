891175c93d47b2952595a1313923dcd725670871
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-08-15 12:07:57 -0400

    User can create new panels

--- app/controllers/panels_controller.rb
+++ app/controllers/panels_controller.rb
@@ -1,3 +1,8 @@
 class PanelsController < ApplicationController
-  
+  def create
+    @panel = Panel.create(name: "New Panel", user_id: current_user.id)
+
+    # TODO: Render current page...or maybe it actually is going somewhere?
+    redirect_via_turbolinks_to root_path
+  end
 end
